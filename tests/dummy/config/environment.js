7ca5d510f10ade5e532338add10eb31200f768d4
Ivan Terpugov <ivan@diplomero.com>
2015-11-10 16:58:01 +0300

    gh pages setup

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -26,6 +26,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
+  if (environment === 'production') {
+    ENV.baseURL = '/ember-cli-vu-meter'
+  }
 
   if (environment === 'test') {
     // Testem prefers this...
